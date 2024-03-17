8b6de710bd110617a7a4e966f34ddb8da79afdf1
kcollett <karen.collett@gmail.com>
2024-03-17 16:54:48 -0500

    Add some options recommended in a standard recipe.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,14 @@ return {
     "folke/tokyonight.nvim",
     lazy = true,
     -- transparent makes background match terminal background (whatever that is)
-    opts = { style = "night", transparent = true },
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
